d2cd60d6bd98f67ae4216d3871b42cf411114cca
SiRipo92 <ripochesierra@gmail.com>
2024-04-15 16:17:48 +0200

    installed swiper node_module and enqueud stylesheets and created files for my mixins and animations

--- wp-content/themes/foce-child/functions.php
+++ wp-content/themes/foce-child/functions.php
@@ -5,16 +5,24 @@ function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
     // Add the parallax style
-    wp_enqueue_style( 'custom-style', get_stylesheet_directory_uri() . '/sass/custom.css' );
+    wp_enqueue_style( 'custom-style', get_stylesheet_directory_uri() . '/style/css/custom.css' );
 
     // Add the Swiper CSS
-    wp_enqueue_style( 'swiper-bundle', get_template_directory_uri() . '-child/js/swiper-bundle.min.js', array(), '10.0.0', true);
+    wp_enqueue_style( 'swiper-bundle-css', get_stylesheet_directory_uri() . '/node_modules/swiper/swiper-bundle.min.css', array(), true );
+    wp_enqueue_script('swiper-bundle-js', get_stylesheet_directory_uri() . '/node_modules/swiper/swiper-bundle.min.js', array(), true);
 
     // Add jQuery from a CDN
-    wp_enqueue_script( 'jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js', array('jquery'), '3.7.1', true );
-
-    // Add jQuery Parallax as a dependency to jQuery
-    wp_enqueue_script('jquery-parallax', 'https://cdnjs.cloudflare.com/ajax/libs/jquery-parallax/1.1.3/jquery-parallax-min.js', array('jquery'), '1.1', true);
+    wp_enqueue_script('jquery');
+        // Add a custom script to check if jQuery is loaded
+        wp_add_inline_script('jquery', '
+            // Check if jQuery is defined
+            if (typeof jQuery !== "undefined") {
+                // jQuery is loaded
+                console.log("jQuery is installed in WordPress.");
+            } else {
+                // jQuery is not loaded
+                console.log("jQuery is NOT installed in WordPress.");
+            }');
 
     // Add custom script
     wp_enqueue_script( 'custom-script', get_stylesheet_directory_uri() . '/js/custom.js', array('jquery'), '1.0.0', true );
