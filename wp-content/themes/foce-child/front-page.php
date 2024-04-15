60190df9c097903a51bdc01d2a2359d5233e0143
SiRipo92 <ripochesierra@gmail.com>
2024-04-16 00:12:22 +0200

    set up structure for parallax window and template-partials for 'page-characters.php' and 'page-oscars'. Also changed logo image name.

--- wp-content/themes/foce-child/front-page.php
+++ wp-content/themes/foce-child/front-page.php
@@ -5,53 +5,25 @@ get_header();
 
     <main id="primary" class="site-main">
         <section class="banner">
-            <div class="parallax-container">
-                <img src="<?php echo get_theme_file_uri(). '/assets/logo.png'; ?> " alt="logo Fleurs d'oranger & chats errants" id="logo">
+            <div class="parallax-wrapper">
+                <video class="banner-video" role="banner-video" autoplay muted loop>
+                    <source src="<?php echo get_stylesheet_directory_uri() . '/assets/videos/Studio+Koukaki-video.mp4'; ?>" type="video/mp4">
+                </video>
+                <img role="banner-logo" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/logo.png'; ?> " alt="logo Fleurs d'oranger & chats errants">
             </div>
         </section>
+
+
         <section id="#story" class="story">
             <h2>L'histoire</h2>
             <article id="" class="story__article">
                 <p><?php echo get_theme_mod('story'); ?></p>
             </article>
-            <?php
-            $args = array(
-                'post_type' => 'characters',
-                'posts_per_page' => -1,
-                'meta_key'  => '_main_char_field',
-                'orderby'   => 'meta_value_num',
 
-            );
-            $characters_query = new WP_Query($args);
-            ?>
-            <article id="characters">
-                <div class="main-character">
-                    <h3>Les personnages</h3>
-                    <?php
-                    $main_character = $characters_query->posts[0];
-                    echo '<figure>';
-                    echo get_the_post_thumbnail( $main_character->ID, 'full' );
-                    echo '<figcaption>'. $main_character->post_title . '</figcaption>';
-                    echo '</figure>';
-                    $characters_query->next_post();
-                    ?>
-                </div>
-                <div class="other-characters">
-                    <?php
-                    while ( $characters_query->have_posts() ) {
-                        $characters_query->the_post();
-                        echo '<figure>';
-                        echo get_the_post_thumbnail( get_the_ID(), 'full' );
-                        echo '<figcaption>';
-                        the_title();
-                        echo'</figcaption>';
-                        echo '</figure>';
-                    }
-                    ?>
-                </div>
-            </article>
+            <?php include(get_stylesheet_directory() . '/template-partials/page-characters.php');?>;
+
             <article id="place">
-                <div>
+                <div>∑
                     <h3>Le Lieu</h3>
                     <p><?php echo get_theme_mod('place'); ?></p>
                 </div>
@@ -68,6 +40,7 @@ get_header();
             </div>
             </section>
     </main><!-- #main -->
-
+<section id="oscars">
+    <?php include(get_stylesheet_directory().'/template-partials/page-oscars.php'); ?>
 <?php
 get_footer();
