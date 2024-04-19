ff8b03f135ece930e8e7c2852e1306834be6a8bc
SiRipo92 <ripochesierra@gmail.com>
2024-04-19 09:50:44 +0200

    added commentary to  divide sections, changed classes, added more divs in the banner, changed class names in hero header, story, the way the files are called, and added spans around the titles. I've added classes for animation areas as well as items to observe and animate.

--- wp-content/themes/foce-child/front-page.php
+++ wp-content/themes/foce-child/front-page.php
@@ -2,47 +2,54 @@
 
 get_header();
 ?>
-
-    <main id="primary" class="site-main">
-        <section class="banner">
-            <div class="video-container">
-                <video class="banner-video" autoplay muted loop>
+<!-- ************************************ SECTION HERO BANNER -->
+<main id="primary" class="site-main">
+    <section class="hero-header">
+        <div class="banner-container">
+            <div class = "banner">
+                <video id= "bg-video" class="banner__video" autoplay muted loop>
                     <source src="<?php echo get_stylesheet_directory_uri() . '/assets/videos/Studio+Koukaki-video.mp4'; ?>" type="video/mp4">
                 </video>
             </div>
-            <div class= "banner-logo" id="banner-logo">
-                <img id="floating-logo" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/logo.png'; ?> " alt="logo Fleurs d'oranger & chats errants">
+            <div class= "hero-header__logo">
+                <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/logo.png'; ?> " alt="logo Fleurs d'oranger & chats errants">
             </div>
-        </section>
+    </section>
+<!-- ************************************ SECTION STORY-->
 
-
-        <section id="story" class="story">
-            <h2>L'histoire</h2>
-            <article id="" class="story__article">
+<!--  STORY__ARTICLE -->
+    <section id="story" class="story">
+        <article id="story__article" class="story__article animation-item">
+                <h2 class="observed-item"><span class ="story__title">L'histoire</span></h2>
                 <p><?php echo get_theme_mod('story'); ?></p>
-            </article>
+        </article>
+        
+<!--  STORY_CHARACTERS / SWIPER SLIDER TEMPLATE  -->    
+<?php get_template_part ('template-partials/page-characters');?>
 
-            <?php include(get_stylesheet_directory() . '/template-partials/page-characters.php');?>;
-
-            <article id="place">
-                <div>
-                    <h3>Le Lieu</h3>
-                    <p><?php echo get_theme_mod('place'); ?></p>
-                </div>
-
-            </article>
-        </section>
-
-
-        <section id="studio">
-            <h2>Studio Koukaki</h2>
-            <div>
-                <p>Acteur majeur de l’animation, Koukaki est un studio intégré fondé en 2012 qui créé, produit et distribue des programmes originaux dans plus de 190 pays pour les enfants et les adultes. Nous avons deux sections en activité : le long métrage et le court métrage. Nous développons des films fantastiques, principalement autour de la culture de notre pays natal, le Japon.</p>
-                <p>Avec une créativité et une capacité d’innovation mondialement reconnues, une expertise éditoriale et commerciale à la pointe de son industrie, le Studio Koukaki se positionne comme un acteur incontournable dans un marché en forte croissance. Koukaki construit chaque année de véritables succès et capitalise sur de puissantes marques historiques. Cette année, il vous présente “Fleurs d’oranger et chats errants”.</p>
+<!--  STORY__PLACE -->
+        <article id="place" class="place animation-item">
+            <div class="place__text">
+                <h3><span class= "place__title">Le Lieu</span></h3>
+                <p><?php echo get_theme_mod('place'); ?></p>
             </div>
-            </section>
-    </main><!-- #main -->
-<section id="oscars">
-    <?php include(get_stylesheet_directory().'/template-partials/page-oscars.php'); ?>
-<?php
+        </article>
+    </section>
+
+<!-- ************************************ SECTION STUDIO--> 
+
+    <section id="studio" class= "animation-item">
+        <h2 class="observed-item"><span class="studio__title">Studio Koukaki<span></h2>
+        <div>
+            <p>Acteur majeur de l’animation, Koukaki est un studio intégré fondé en 2012 qui créé, produit et distribue des programmes originaux dans plus de 190 pays pour les enfants et les adultes. Nous avons deux sections en activité : le long métrage et le court métrage. Nous développons des films fantastiques, principalement autour de la culture de notre pays natal, le Japon.</p>
+            <p>Avec une créativité et une capacité d’innovation mondialement reconnues, une expertise éditoriale et commerciale à la pointe de son industrie, le Studio Koukaki se positionne comme un acteur incontournable dans un marché en forte croissance. Koukaki construit chaque année de véritables succès et capitalise sur de puissantes marques historiques. Cette année, il vous présente “Fleurs d’oranger et chats errants”.</p>
+        </div>
+    </section>
+
+
+<!--    STUDIO__NOMINATION -->
+<?php get_template_part ('template-partials/page-nomination.php'); ?>
+
+</main>
+<?php 
 get_footer();
