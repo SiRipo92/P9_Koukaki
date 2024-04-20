0a83ef7f74b9d8c4abb22807e0093a814f7274ba
SiRipo92 <ripochesierra@gmail.com>
2024-04-20 16:45:00 +0200

    Finished structuring the php templates for oscars nomination and started styling characters slide. The images and structure are currently functional but haven't taken into account media queries yet

---
+++ wp-content/themes/foce-child/template-partials/page-nomination.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template part for displaying page content in page.php
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Fleurs_d\'oranger_&_Chats_errants
+ */
+
+?>
+<section id= "#nomination" class="nomination">
+    <div class="nomination__title">
+        <span><h3 class="oscars-content"> Fleurs d'oranger & chats errants est nominé aux Oscars Short Film Animated de 2022 !</span></h3>
+    </div>
+    <div class="nomination__image">
+        <img src="<?php echo get_stylesheet_directory_uri().'/assets/images/oscars-2021.png'; ?>" alt="Nominated for an Oscar for Animated Short Films" class="oscars_img">
+    </div>
+</section>
