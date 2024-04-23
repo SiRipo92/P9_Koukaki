815dd51a0003c4ccf360234b11d3e91c11d001d5
SiRipo92 <ripochesierra@gmail.com>
2024-04-23 13:55:56 +0200

    changed format of swiper's page-characters.php to use swiper named divs

--- wp-content/themes/foce-child/template-partials/page-characters.php
+++ wp-content/themes/foce-child/template-partials/page-characters.php
@@ -1,31 +1,32 @@
-
 <!-- ************************************ SECTION STORY SWIPER SLIDER -->
-<?php
-    $args = array(
-        'post_type' => 'characters',
-        'posts_per_page' => -1,
-        'meta_key'  => '_main_char_field',
-        'orderby'   => 'meta_value_num',
-
-    );
-    $characters_query = new WP_Query($args);
-    ?>
-<article id="characters" class="animation-item">
-        <h3><span class ="characters__title">Les personnages</span></h3>
-        <div class="swiper-main-container">
-            <?php
-            while ( $characters_query->have_posts() ) {
-                $characters_query->the_post();
-                // Insert slides here
-                echo '<div class="swiper-slide">';
-                echo '<figure>';
-                echo get_the_post_thumbnail( get_the_ID(), 'full' );
-                echo '<figcaption>';
-                the_title();
-                echo'</figcaption>';
-                echo '</figure>';
-                echo '</div>';
-            }
-            ?>
-        </div>
-</article>
+<div class="animation-item swiper-container">
+    <div class ="swiper mySwiper"> 
+        <div class="swiper-wrapper">
+            <!-- CHARACTER 1 -->
+            <div class="swiper-slide">
+                <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/characters/Kawaneko.png'; ?>" alt="hello purple cat">
+                <h4>Kawaneko</h4>
+            </div>
+            <!-- CHARACTER 2 -->
+            <div class="swiper-slide">
+                <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/characters/Orenjiiro.png'; ?>" alt="hello orange cat">
+                <h4>Orenjiiro</h4>
+            </div>
+            <!-- CHARACTER 3 -->
+            <div class="swiper-slide">
+                <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/characters/Pinku.png'; ?>" alt="hello white cat">
+                <h4>Pinku</h4>
+            </div>
+            <!-- CHARACTER 4 -->
+            <div class="swiper-slide">
+                <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/characters/Tenshi.png'; ?>" alt="hello orange and yellow cat">
+                <h4>Tenshi</h4>
+            </div>
+            <!-- CHARACTER 5 -->
+            <div class="swiper-slide">
+                <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/characters/Jaakuna.png'; ?>" alt="hello grey cat">
+                <h4>Jaakuna</h4>
+            </div>
+        </div>    
+    </div>
+</div>
