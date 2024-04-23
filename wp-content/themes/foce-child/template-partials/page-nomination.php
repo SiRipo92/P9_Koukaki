ccb7d4e6f11ccaf3f7520f8d0ea190f591b23eae
SiRipo92 <ripochesierra@gmail.com>
2024-04-23 13:56:22 +0200

    moved span tag after h3 tag

--- wp-content/themes/foce-child/template-partials/page-nomination.php
+++ wp-content/themes/foce-child/template-partials/page-nomination.php
@@ -10,7 +10,7 @@
 ?>
 <section id= "#nomination" class="nomination">
     <div class="nomination__title">
-        <span><h3 class="oscars-content"> Fleurs d'oranger & chats errants est nominé aux Oscars Short Film Animated de 2022 !</span></h3>
+        <h3 class="oscars-content"><span class="oscars__title"> Fleurs d'oranger & chats errants est nominé aux Oscars Short Film Animated de 2022 !</span></h3>
     </div>
     <div class="nomination__image">
         <img src="<?php echo get_stylesheet_directory_uri().'/assets/images/oscars-2021.png'; ?>" alt="Nominated for an Oscar for Animated Short Films" class="oscars_img">
