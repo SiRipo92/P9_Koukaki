6cf7c75ef717794c77cd6abd5601cbe4a318f8a3
SiRipo92 <ripochesierra@gmail.com>
2024-04-23 13:57:53 +0200

    placed interesection observers in jquery document ready functions

--- wp-content/themes/foce-child/js/custom.js
+++ wp-content/themes/foce-child/js/custom.js
@@ -1,29 +1,30 @@
 (function($) {
 
   // Make sections appear on scroll
-  const sectionObserver = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('fade-in');
-      } else {
-        if (entry.target) {
-          entry.target.classList.remove('fade-in');
+  $(document).ready(function() {
+    const sectionObserver = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          $(entry.target).addClass('fade-in');
+        } else {
+          $(entry.target).removeClass('fade-in');
         }
-      }
-    });
-  }, { threshold: 0.2 });
+      });
+    }, { threshold: 0.4 });
+  
+    document.querySelectorAll("section, banner, .story, #characters, #place, .studio, #nomination, .site-footer").forEach(sectionSite => sectionObserver.observe(sectionSite));
+  });
 
-  document.querySelectorAll(".animation-item").forEach(sectionInSite => sectionObserver.observe(sectionInSite));
-
+  // Make titles appear on scroll
   const titlesObserver = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        entry.target.classList.add('title-anim');
+        $(entry.target).addClass('title-anim');
       }
     });
-  }, { threshold: 0.9 });
+  }, { threshold: 0.5 });
 
-  document.querySelectorAll("h2 > span").forEach(sectionTitle => titlesObserver.observe(sectionTitle));
+  document.querySelectorAll("h2 span, h3 span, h4 span").forEach(sectionTitle => titlesObserver.observe(sectionTitle));
 
   // Parallax effect
   $(window).scroll(function () {
@@ -31,39 +32,45 @@
     $('.hero-header__logo').css({'transform': 'translate(-50%, -50%) translateY(' + ($(this).scrollTop() * 0.7) + 'px)'});
   });
 
-  // Import Swiper
-  document.addEventListener('DOMContentLoaded', function() {
-    const swiper = new Swiper('.swiper-main-container', {
-      slidesPerView: "auto",
-      effect: "coverflow",
-      spaceBetween: 20, // Space between slides (default: 0)
-      loop: true,// infinite looping (default: false)
-      grabCursor: true,
-      centeredSlides: true,
-      loopAdditionalSlides: 1,
-      autoplay: {
-        delay: 2000
-      }, 
-      coverflowEffect: {
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows: true,
-      },
-      pagination: {
-        el: '.swiper-pagination', // Selector for pagination element (optional)
-      },
-      breakpoints: {
-        // Optional - Set different options for different screen sizes
-        640: {
-          slidesPerView: 2, 
-        },
-        1024: {
-          slidesPerView: 3, 
-        },
+  $(document).ready(function() {
+    var swiper;
+  
+    function initSwiper() {
+      var swiperContainer = document.querySelector('.mySwiper');
+      if (swiperContainer) {
+        swiper = new Swiper('.mySwiper', {
+          loop: true,
+          spaceBetween: 100,
+          autoplay: true, 
+          speed: 1000,
+          effect: 'coverflow',
+          grabCursor: true,
+          centeredSlides: true,
+          slidesPerView: 3,
+          coverflowEffect: {
+            rotate: 0,
+            stretch: 80,
+            depth: 200,
+            modifier: 1,
+            slideShadows: false,
+          },
+        });
+      }
+    }
+  
+    // Initiates the swiper slider
+    // We wrap the initSwiper function call inside a window load event listener
+    // This ensures that the Swiper library is fully loaded before we try to use it
+    window.addEventListener('load', function() {
+      initSwiper();
+    });
+  
+    $(window).resize(function() {
+      if (swiper) {
+        swiper.destroy(); // Destroy the swiper instance
+        initSwiper(); // Re-initiate the swiper according to the width of the screen
       }
     });
-  }); 
+  });
 
-})(jQuery); // Added the missing closing parenthesis here
+})(jQuery);
