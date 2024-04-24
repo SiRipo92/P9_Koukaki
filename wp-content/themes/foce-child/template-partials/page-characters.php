2fca342a166c99fd161a0692b76a5524450a9246
SiRipo92 <ripochesierra@gmail.com>
2024-04-24 11:21:27 +0200

    changed template format for swiper and removed h4 titles for characters' names

--- wp-content/themes/foce-child/template-partials/page-characters.php
+++ wp-content/themes/foce-child/template-partials/page-characters.php
@@ -1,32 +1,28 @@
 <!-- ************************************ SECTION STORY SWIPER SLIDER -->
-<div class="animation-item swiper-container">
-    <div class ="swiper mySwiper"> 
-        <div class="swiper-wrapper">
-            <!-- CHARACTER 1 -->
-            <div class="swiper-slide">
-                <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/characters/Kawaneko.png'; ?>" alt="hello purple cat">
-                <h4>Kawaneko</h4>
-            </div>
-            <!-- CHARACTER 2 -->
-            <div class="swiper-slide">
-                <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/characters/Orenjiiro.png'; ?>" alt="hello orange cat">
-                <h4>Orenjiiro</h4>
-            </div>
-            <!-- CHARACTER 3 -->
-            <div class="swiper-slide">
-                <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/characters/Pinku.png'; ?>" alt="hello white cat">
-                <h4>Pinku</h4>
-            </div>
-            <!-- CHARACTER 4 -->
-            <div class="swiper-slide">
-                <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/characters/Tenshi.png'; ?>" alt="hello orange and yellow cat">
-                <h4>Tenshi</h4>
-            </div>
-            <!-- CHARACTER 5 -->
-            <div class="swiper-slide">
-                <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/characters/Jaakuna.png'; ?>" alt="hello grey cat">
-                <h4>Jaakuna</h4>
-            </div>
-        </div>    
+    <div class="animation-item swiper-container">
+        <div class ="swiper mySwiper"> 
+            <div class="swiper-wrapper">
+                <!-- CHARACTER 1 -->
+                <div class="swiper-slide">
+                    <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/characters/Kawaneko.png'; ?>" alt="hello purple cat">
+                </div>
+                <!-- CHARACTER 2 -->
+                <div class="swiper-slide">
+                    <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/characters/Orenjiiro.png'; ?>" alt="hello orange cat">
+                </div>
+                <!-- CHARACTER 3 -->
+                <div class="swiper-slide">
+                    <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/characters/Pinku.png'; ?>" alt="hello white cat">
+                </div>
+                <!-- CHARACTER 4 -->
+                <div class="swiper-slide">
+                    <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/characters/Tenshi.png'; ?>" alt="hello orange and yellow cat">
+                </div>
+                <!-- CHARACTER 5 -->
+                <div class="swiper-slide">
+                    <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/characters/Jaakuna.png'; ?>" alt="hello grey cat">
+                </div>
+            </div>    
+        </div>
     </div>
-</div>
+</article>
