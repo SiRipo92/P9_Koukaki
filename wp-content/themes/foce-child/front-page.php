aadcc633a0960970882e85883d7cfaf03b008fe6
SiRipo92 <ripochesierra@gmail.com>
2024-05-02 15:07:47 -0400

    Added class 'title' to H2 and H3

--- wp-content/themes/foce-child/front-page.php
+++ wp-content/themes/foce-child/front-page.php
@@ -23,20 +23,20 @@ get_header();
 <!--  STORY__ARTICLE -->
     <section id="story" class="story hidden">
         <article id="story__article" class="story__article">
-                <h2><span>L'histoire</span></h2>
+                <h2><span class="title">L'histoire</span></h2>
                 <p><?php echo get_theme_mod('story'); ?></p>
         </article>
         
 <!--  STORY_CHARACTERS / SWIPER SLIDER TEMPLATE  -->  
         <article id="characters-swiper" class="story__characters">
-            <h3><span>Les personnages</span></h3>  
+            <h3><span class="title">Les personnages</span></h3>  
             <?php get_template_part ('template-partials/page-characters');?>
 <!--  STORY__PLACE -->
 
         <article id="place" class="place">
 
             <div>
-                <h3><span>Le Lieu</span></h3>
+                <h3><span class="title">Le Lieu</span></h3>
                 <p><?php echo get_theme_mod('place'); ?></p>
             </div>
             <!----- Insert cloud images here -------->
@@ -49,7 +49,7 @@ get_header();
 <!-- ************************************ SECTION STUDIO--> 
 
 <section id="studio" class= "studio hidden">
-    <h2><span>Studio Koukaki</span></h2>
+    <h2><span class="title">Studio Koukaki</span></h2>
     <div>
         <p>Acteur majeur de l’animation, Koukaki est un studio intégré fondé en 2012 qui créé, produit et distribue des programmes originaux dans plus de 190 pays pour les enfants et les adultes. Nous avons deux sections en activité : le long métrage et le court métrage. Nous développons des films fantastiques, principalement autour de la culture de notre pays natal, le Japon.</p>
         <p>Avec une créativité et une capacité d’innovation mondialement reconnues, une expertise éditoriale et commerciale à la pointe de son industrie, le Studio Koukaki se positionne comme un acteur incontournable dans un marché en forte croissance. Koukaki construit chaque année de véritables succès et capitalise sur de puissantes marques historiques. Cette année, il vous présente “Fleurs d’oranger et chats errants”.</p>
